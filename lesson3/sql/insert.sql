6a811e0a22a7e3672f51983fb4df55635c49851e
Aliazhari <Aliazhari@hotmail.com>
2019-01-20 05:50:10 -0500

    Inserting records in table courses courses in insert.sql - lesson3

--- lesson3/sql/insert.sql
+++ lesson3/sql/insert.sql
@@ -4,3 +4,12 @@ INSERT INTO students ( lastname, firstname, email, age, ssn) VALUES ('Birm', 'Su
 INSERT INTO students ( lastname, firstname, email, age, ssn) VALUES ('Mooney', 'Debra', 'debra@mooney.com', 24, '444444444');
 INSERT INTO students ( lastname, firstname, email, age, ssn) VALUES ('Adel', 'Ali', 'ali@adel.com', 20, '555555555');
 INSERT INTO students ( lastname, firstname, email, age, ssn) VALUES ('Gonzales', 'Mary', 'maryn@gonzales.com', 22, '666666666');
+
+# Inserting in table courses
+
+INSERT INTO courses ( name, number, description) VALUES ('Python', 'CS120', 'A web development with Python and Javascript. This courses covers SQL, Git and many other technologies');
+INSERT INTO courses ( name, number, description) VALUES ('Introduction to Java', 'CS130', 'An Introduction to Java. Covers data types, conditions, loop and classes');
+INSERT INTO courses ( name, number, description) VALUES ('Data structures', 'CS250', 'First half of the term covers Data structures: Arrays, Linklist, Verctors, HashMap, Hashtable..etc');
+INSERT INTO courses ( name, number, description) VALUES ('Modern History', 'HIS320', 'An Introduction to mocern history. Covers world war I and II');
+INSERT INTO courses ( name, number, description) VALUES ('Introduction to Psychology', 'PSY101', 'An introduction to Psychology');
+INSERT INTO courses ( name, number, description) VALUES ('Calculus I', 'MATh271', 'Introduction to calculus. Covers derivitives and integrals');
