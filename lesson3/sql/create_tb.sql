9355e46729a52b484763663c56adbbfc7013dcee
Aliazhari <Aliazhari@hotmail.com>
2019-01-20 08:56:19 -0500

    Creating table staff in create_tb.sql - lesson3

--- lesson3/sql/create_tb.sql
+++ lesson3/sql/create_tb.sql
@@ -1,3 +1,5 @@
+# Creating table students
+
 CREATE TABLE students (
  id INT NOT NULL AUTO_INCREMENT,
  lastname VARCHAR(20) NOT NULL,
@@ -8,6 +10,8 @@ CREATE TABLE students (
  primary key (id)
  );
 
+# Creating table courses
+
  CREATE TABLE courses (
  id INT NOT NULL AUTO_INCREMENT,
  name VARCHAR(50) NOT NULL,
@@ -15,3 +19,19 @@ CREATE TABLE students (
  description VARCHAR(255) NOT NULL,
  primary key (id)
  );
+
+
+ # Creating table staff
+
+ CREATE TABLE staff (
+ id INT NOT NULL AUTO_INCREMENT,
+ lastname VARCHAR(20) NOT NULL,
+ firstname VARCHAR(20) NOT NULL,
+ email VARCHAR(50) NOT NULL,
+ age SMALLINT NOT NULL,
+ ssn VARCHAR(10) NOT NULL UNIQUE,
+ primary key (id)
+ );
+
+
+
