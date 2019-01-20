0e4efd1496dde49413930dfb1032410fa42e18ca
Aliazhari <Aliazhari@hotmail.com>
2019-01-20 04:32:17 -0500

    Creating insert.sql - lesson3

---
+++ lesson3/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO students ( lastname, firstname, email, age, ssn) VALUES ('Smith', 'John', 'john@smith.com', 22, '111111111');
+INSERT INTO students ( lastname, firstname, email, age, ssn) VALUES ('Milky', 'Robert', 'robert@milky.com', 23, '222222222');
+INSERT INTO students ( lastname, firstname, email, age, ssn) VALUES ('Birm', 'Susan', 'susan@Birm.com', 21, '333333333');
+INSERT INTO students ( lastname, firstname, email, age, ssn) VALUES ('Mooney', 'Debra', 'debra@mooney.com', 24, '444444444');
+INSERT INTO students ( lastname, firstname, email, age, ssn) VALUES ('Adel', 'Ali', 'ali@adel.com', 20, '555555555');
+INSERT INTO students ( lastname, firstname, email, age, ssn) VALUES ('Gonzales', 'Mary', 'maryn@gonzales.com', 22, '666666666');
