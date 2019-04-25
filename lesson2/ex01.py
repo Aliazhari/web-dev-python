3c187f9102d61044a109169967ee588e19c76bc3
Aliazhari <Aliazhari@hotmail.com>
2019-04-25 09:59:00 -0400

    lesson 2, ex01, ex02 and ex03

---
+++ lesson2/ex01.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+# before you run flask run in th terminal, you need to set the envirement variable FLASK_APP
+# export FLASK_APP=ex01.py (or whatever this file name is)
+
+# if you get an error running flask that says adress is already in use,
+# you need to kill the process first.
+# first to find the process type in terminal:
+# $ sudo lsof -i:5000
+# kill XXXXX
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello, world!"
+
