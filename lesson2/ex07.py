a8132ffbd2166bf141d82de0c96fb006c4c3af97
Aliazhari <Aliazhari@hotmail.com>
2019-04-25 13:15:06 -0400

    ex07 is it my birthday yet

---
+++ lesson2/ex07.py
@@ -0,0 +1,23 @@
+import datetime
+from flask import Flask, render_template
+
+# before you run flask run in th terminal, you need to set the envirement variable FLASK_APP
+# export FLASK_APP=ex01.py (or whatever this file name is)
+
+# if you get an error running flask that says adress is already in use,
+# you need to kill the process first.
+# first to find the process type in terminal:
+# $ sudo lsof -i:5000
+# kill XXXXX
+
+# to avoid such error, make sure you terminate your flask application by CTRL + C
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    now = datetime.datetime.now()
+    new_year = now.month == 4 and now.day == 25
+    return render_template("index07.html", new_year=new_year)
+
